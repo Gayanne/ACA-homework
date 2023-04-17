bac216f9681adcdbadead41656bfe2e09eda55b1
Gayane Nersisyan <gayane.nersisyan1@gmail.com>
2023-04-17 19:39:07 +0400

    added collor to backround

--- scripts (7)/scripts/script.js
+++ scripts (7)/scripts/script.js
@@ -2,22 +2,29 @@ const people = [{
     name: "Shakira",
     info: "singer",
     image: "https://www.shakira.com/uploaded/gallery/thumbs/3e9592b03c5e108e5b83c749047ff8eb.jpg",
-    link:"another.html"
+    link:"another.html",
+    ismyturn: true
 }, {
     name: "Jeniffer",
     info: "singer",
     image: "https://media.gettyimages.com/id/1141979804/photo/jennifer-lopez-on-monday-may-6-2019.jpg?s=612x612&w=gi&k=20&c=lre4-TTex_gw-YG3DE5CCraqmULA5aCnOR5nSkoFA64="
-   , link:"contact.html"
+   , link:"contact.html",
+     ismyturn: false
 }, {
     name: "Rihana",
     info: "singer",
     image: "https://upload.wikimedia.org/wikipedia/commons/c/c2/Rihanna_Fenty_2018.png",
-    link:"about.html"
+    link:"about.html",
+    ismyturn: false,
+
 }]
 const root = document.querySelector('body');
 for (let person of people) {
     const Container = document.createElement('div');
     Container.className = "person";
+    if(person.ismyturn){
+        Container.id = "highlight"
+    }
 
     const link=document.createElement('a');
     link.href=person.link;
@@ -25,14 +32,14 @@ for (let person of people) {
     const photo = document.createElement('img');
     photo.src = person.image;
     link.appendChild(photo);
-    
+    Container.appendChild(link);
 
 
     const Name = document.createElement("h1");
     Name.innerHTML = person.name;
     Container.appendChild(Name);
 
-    Container.appendChild(link);
+    
     root.appendChild(Container);
 
 } 
